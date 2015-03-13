8715a44c63b4f4b55d71e411ba0b74abfd97ec0b
Daniel Fernando Escobar Arenas <danesco0507@gmail.com>
2015-03-13 09:35:45 -0500

    Se agrega buscador por fechas y nombre

--- work_notifier/urls.py
+++ work_notifier/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,5 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^notifier/', include('notifier.urls'))
+    url(r'^notifier/', include('notifier.urls')),
+
+    url(r'^$', RedirectView.as_view(url='/notifier/')),
 )
